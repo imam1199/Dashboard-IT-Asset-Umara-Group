3fa86f47378376577c86ab28c421892ce91cac30
imam1199 <imam.wb.tkj1@gmail.com>
2026-06-05 11:18:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import gspread
 import pandas as pd
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 st.set_page_config(page_title="Dashboard IT Asset", layout="wide")
@@ -8,29 +9,14 @@ st.set_page_config(page_title="Dashboard IT Asset", layout="wide")
 @st.cache_resource
 def get_gspread_client():
     try:
-        # Mengambil key dari secrets dan memperbaiki format \n
-        raw_key = st.secrets["GCP_PRIVATE_KEY"]
-        private_key = raw_key.replace("\\n", "\n")
-        
-        creds_dict = {
-            "type": "service_account",
-            "project_id": "dashboard-laptop-it",
-            "private_key_id": "f2733e150461a3dacf6af401943b3dafbe098bf4",
-            "private_key": private_key, 
-            "client_email": "dashboard-it-asset@dashboard-laptop-it.iam.gserviceaccount.com",
-            "client_id": "112552009541399448470",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dashboard-it-asset%40dashboard-laptop-it.iam.gserviceaccount.com"
-        }
+        # Mengambil JSON utuh dari secrets
+        creds_dict = json.loads(st.secrets["GCP_JSON"])
         
         scope = [
             'https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive',
             'https://www.googleapis.com/auth/spreadsheets'
         ]
-        
         creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
         return gspread.authorize(creds)
     except Exception as e:
